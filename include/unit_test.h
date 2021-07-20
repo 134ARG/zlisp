07284a0f4fba94c18a1373bba8e4c77c7f002c1b
134arg <xen134@outlook.com>
2021-07-20 23:40:38 +0800

    zlisp: test: add ASSERT macro

--- include/unit_test.h
+++ include/unit_test.h
@@ -5,9 +5,17 @@
 #ifndef ZLISP_UNIT_TEST_H
 #define ZLISP_UNIT_TEST_H
 
+#include "logger.h"
 #include "status.h"
+
+#define ASSERT(COND)                                                           \
+  do {                                                                         \
+    if (!(COND)) {                                                             \
+      LOG_ERROR("failed: %s\n", #COND);                                        \
+    }                                                                          \
+  } while (0)
 
 enum status test_lexer();
 void test_list();
 
-#endif //ZLISP_UNIT_TEST_H
+#endif // ZLISP_UNIT_TEST_H
