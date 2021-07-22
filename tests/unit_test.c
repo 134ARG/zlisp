2ea7cf3c162c784b7e13cb1a1259a57460b8ac90
zry <zry@naive.systems>
2021-07-22 09:22:29 +0800

    zlisp: test: fix segfault

--- tests/unit_test.c
+++ tests/unit_test.c
@@ -14,15 +14,15 @@ enum status
 test_lexer()
 {
 	struct file_context test_file = file_context_new(
-			"/home/xen134/zlisp/cmake-build-debug/test.file", "r");
+			"./test.ll", "r");
 	if (!test_file.file) {
 		printf("Hello, World!\n");
 		return 0;
 	}
 	string segment = read_segment(&test_file);
 	while (segment.size != 0) {
-		fprintf(stderr, "%s, ", segment.data);
-		string_clean(&segment);
+		fprintf(stderr, "%s | ", segment.data);
+		string_free(&segment);
 		segment = read_segment(&test_file);
 	}
 	return OK;
